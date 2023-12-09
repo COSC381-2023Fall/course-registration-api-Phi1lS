df8072fae95871a6f66a76e98ec08a1acdedc1d5
Phill Solis <psolis@emich.edu>
2023-12-08 22:14:41 -0500

    Show a student's registered course
    main.py was modified to add method to register students to a class in FastAPI app.
    
    student.py was created to create a Student Object, which takes eid and name. Courses can be added using the FastAPI app.
    
    test_main.py was edited to test all new FastAPI methods.
    
    test_student.py was created to test all student methods.

--- test_main.py
+++ test_main.py
@@ -1,24 +1,63 @@
 from fastapi.testclient import TestClient
-from main import app
-from test_course import courseA
-from course import courses
+from main import app, students
+from course import Course, courses
+from student import Student
 
 client = TestClient(app)
 
-def test_get_courses(courseA):
+def test_get_courses():
+    courseA = Course("COSC", "111", 30, "John Doe", "Programming I", "PH 503", "TH 9:00")
     courses.append(courseA)
-
+    
     response = client.get("/courses/COSC")
     assert response.status_code == 200
-    assert response.json() == [
-        {
-            "_prefix":"COSC",
-            "_course_number":"111",
-            "_cap":30,
-            "_instructor":"John Doe",
-            "_name":"Programming I",
-            "_place":"PH 503",
-            "_meeting_times":"TH 9:00"
-        }]
+    assert len(response.json()) == 1
+    assert response.json()[0]['prefix'] == 'COSC'
+    assert response.json()[0]['course_number'] == '111'
 
-    courses.pop()
+    courses.remove(courseA)
+
+def test_get_student_courses():
+    student = Student("EID123", "John Doe")
+    courseA = Course("COSC", "111", 30, "John Doe", "Programming I", "PH 503", "TH 9:00")
+    student.register_course(courseA)
+    students["EID123"] = student
+
+    response = client.get("/students/EID123/courses")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+    assert response.json()[0]['prefix'] == 'COSC'
+    assert response.json()[0]['course_number'] == '111'
+
+    del students["EID123"]
+
+def test_register_course_to_student():
+    # Setup - create a student
+    student = Student("EID123", "John Doe")
+    students["EID123"] = student
+
+    # Data for the course to register
+    course_data = {
+        "prefix": "COSC",
+        "course_number": "111",
+        "cap": 30,
+        "instructor": "John Doe",
+        "name": "Programming I",
+        "place": "PH 503",
+        "meeting_times": "TH 9:00"
+    }
+
+    # Make the POST request
+    response = client.post(f"/students/EID123/register_course/", json=course_data)
+
+    # Check the response
+    assert response.status_code == 200
+    assert response.json() == {"message": "Course registered successfully"}
+
+    # Check if the course was actually added to the student
+    assert len(students["EID123"].registered_courses) == 1
+    assert students["EID123"].registered_courses[0]._prefix == "COSC"
+    assert students["EID123"].registered_courses[0]._course_number == "111"
+
+    # Clear memory
+    del students["EID123"]
